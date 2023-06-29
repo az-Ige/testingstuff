7eff1116f6e30e21d3c7abcee1f5dec9c4013e68
Yellow Heliconia <YellowHeliconia@plants.nature>
2023-06-30 01:40:21 +0300

    retesting updates user js

---
+++ test-update.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        google.com: test update
+// @namespace   Violentmonkey Scripts
+// @match       https://www.google.com/
+// @grant       none
+// @updateURL   https://raw.githubusercontent.com/az-Ige/testingstuff/main/test-update.user.js
+// @downloadURL https://raw.githubusercontent.com/az-Ige/testingstuff/main/test-update.user.js
+// @version     1.1
+// @author      -
+// @description 6/30/2023, 1:26:10 AM
+// ==/UserScript==
+
+// must increment version for auto update to work
+
+alert("an update");
