5cf451da55ef7f1ebbe7882e5c15aebd1b6fcded
Yellow Heliconia <YellowHeliconia@plants.nature>
2023-06-30 01:43:30 +0300

    update the update-testscript

--- test-update.user.js
+++ test-update.user.js
@@ -5,11 +5,11 @@
 // @grant       none
 // @updateURL   https://raw.githubusercontent.com/az-Ige/testingstuff/main/test-update.user.js
 // @downloadURL https://raw.githubusercontent.com/az-Ige/testingstuff/main/test-update.user.js
-// @version     1.1
+// @version     1.2
 // @author      -
 // @description 6/30/2023, 1:26:10 AM
 // ==/UserScript==
 
 // must increment version for auto update to work
 
-alert("an update");
+alert("an update 1.2");
